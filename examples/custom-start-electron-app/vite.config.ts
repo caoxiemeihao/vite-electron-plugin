f160da6880a7fbc92b98c8b57514a904a6e32d13
草鞋没号 <308487730@qq.com>
2022-09-28 13:55:36 +0800

    refactor: custom start APP with v0.3.0

--- examples/custom-start-electron-app/vite.config.ts
+++ examples/custom-start-electron-app/vite.config.ts
@@ -1,42 +1,18 @@
 import path from 'path'
 import { rmSync } from 'fs'
 import { defineConfig } from 'vite'
-import electron, { ResolvedConfig } from 'vite-electron-plugin'
+import electron from 'vite-electron-plugin'
+import { customStart } from 'vite-electron-plugin/plugin'
 
 rmSync(path.join(__dirname, 'dist-electron'), { recursive: true, force: true })
-
-let config: ResolvedConfig
-let startup: ResolvedConfig['startup']
 
 export default defineConfig({
   plugins: [electron({
     include: [
       'electron',
     ],
-    configResolved(_config) {
-      config = _config
-      startup = debounce(_config.startup)
-    },
-    onwatch(_event, filepath) {
-      const { extensions, isPreload, viteDevServer } = config
-
-      if (extensions.some(ext => path.extname(filepath))) {
-        if (isPreload(filepath)) {
-          viteDevServer?.ws.send({ type: 'full-reload' })
-        } else {
-          startup(['.', '--no-sandbox'])
-        }
-      }
-    },
-    startup: false,
+    plugins: [customStart((args) => {
+      args?.startup(['.', '--no-sandbox'])
+    })],
   })],
 })
-
-function debounce<Fn extends (...args: any[]) => void>(fn: Fn, delay = 299) {
-  let t: NodeJS.Timeout
-  return ((...args) => {
-    // !t && fn(...args) // first call
-    clearTimeout(t)
-    t = setTimeout(() => fn(...args), delay)
-  }) as Fn
-}
