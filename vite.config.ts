f43fbbb7b31c376c0247c7b4b1fb5ae061783226
草鞋没号 <308487730@qq.com>
2022-10-02 17:28:22 +0800

    choer: ignore warning

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,12 @@ const config: UserConfig = {
         ...builtinModules.map(m => `node:${m}`),
         ...Object.keys(pkg.dependencies),
       ],
+      output: {
+        // Entry module "src/index.ts" is using named and default exports together.
+        // Consumers of your bundle will have to use `chunk["default"]` to access the default export, which may not be what you want.
+        // Use `output.exports: "named"` to disable this warning
+        exports: 'named',
+      },
     },
   },
 }
