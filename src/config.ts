befb032a3dec6dd5adb3c0cc4c9508effa8f9fb0
草鞋没号 <308487730@qq.com>
2022-12-25 11:00:10 +0800

    feat: bump notbundle to 0.3.1, `esbuildPlugin`

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 import path from 'node:path'
 import type { ViteDevServer, ResolvedConfig as ViteResolvedConfig, UserConfig } from 'vite'
-import { startup } from 'vite-plugin-electron'
 import {
   type Configuration as Configuration2,
   type ResolvedConfig as ResolvedConfig2,
   normalizePath,
 } from 'notbundle'
+import { startup } from '.'
 
-export interface Configuration extends Omit<Configuration2, 'output' | 'plugins'> {
+export interface Configuration extends Omit<Configuration2, 'output' | 'plugins' | 'transformOptions'> {
   /** @default 'dist-electron' */
   outDir?: string
   api?: {
@@ -23,14 +23,16 @@ export interface Configuration extends Omit<Configuration2, 'output' | 'plugins'
     // Overwrite `ResolvedConfig`
     configResolved?: (config: ResolvedConfig) => void | Promise<void>
   })[]
+  transformOptions?: import('esbuild').TransformOptions
 }
 
-export interface ResolvedConfig extends Omit<ResolvedConfig2, 'config' | 'output' | 'plugins' | 'experimental'> {
+export interface ResolvedConfig extends Omit<ResolvedConfig2, 'config' | 'output' | 'plugins' | 'experimental' | 'transformOptions'> {
   config: Configuration
   /** Absolute path */
   outDir: string
   api: NonNullable<Configuration['api']>
   plugins: NonNullable<Configuration['plugins']>
+  transformOptions: import('esbuild').TransformOptions
   /** Internal functions (🚨 Experimental) */
   experimental: Omit<ResolvedConfig2['experimental'], 'include2files' | 'include2globs'> & {
     // Overwrite `ResolvedConfig`
@@ -51,7 +53,7 @@ export function polyfillConfig(config: Configuration): Configuration {
     plugins = [],
   } = config
   config.plugins = [<Plugin>{
-    name: ':polyfill-config',
+    name: 'electron:polyfill-config',
     configResolved(_config) {
       _config.outDir = normalizePath(path.isAbsolute(outDir) ? outDir : path.join(_config.root, outDir))
       _config.api = config.api ?? {}
@@ -62,7 +64,33 @@ export function polyfillConfig(config: Configuration): Configuration {
       // @ts-ignore
       // https://github.com/caoxiemeihao/notbundle/blob/v0.2.0/src/config.ts#L22-L26
       _config.output = _config.outDir
+
+      // Use esbuild instead swc.
+      const swc = _config.plugins.findIndex(c => c.name === ':swc')
+      swc !== -1 && _config.plugins.splice(swc, 1, esbuildPlugin(_config))
     },
   }].concat(plugins)
   return config
+}
+
+function esbuildPlugin(config: ResolvedConfig): Plugin {
+  console.log(config.transformOptions)
+  config.transformOptions = {}
+  // Electron only support cjs.
+  config.transformOptions.format ??= 'cjs'
+  config.transformOptions.target ??= 'node14'
+  console.log(config.transformOptions)
+
+  let esbuild: typeof import('esbuild')
+  return {
+    name: 'electron:esbuild',
+    async transform({ filename, code }) {
+      esbuild ??= (await import('esbuild'))
+      const { transformOptions } = config
+      return esbuild.transformSync(code, {
+        loader: path.extname(filename).slice(1) as import('esbuild').Loader,
+        ...transformOptions,
+      })
+    },
+  }
 }
